ffa75a70ce39792370bf56f158104cb485bb9320
Thiago Tavares <obfuscado@proton.me>
2023-04-29 20:50:50 -0300

    Removing code repetition

--- bhaskara.py
+++ bhaskara.py
@@ -9,13 +9,14 @@ delta = b ** 2 - 4 * a * c
 if delta < 0:
     print('Não possui raizes reais.')
 
-elif delta == 0:
+else:
     x1 = (-b + sqrt(delta)) / (2 * a)
-    print(f'A equação possui uma raiz real: {x1}')
-
-else:  
-    x1 = (-b + sqrt(delta)) / (2 * a)
-    x2 = (-b - sqrt(delta)) / (2 * a)
-   
-    print(f"x' = {x1}")
-    print(f"x'' = {x2}")
+    
+    if delta == 0:
+        print(f'A equação possui uma raiz real: {x1}')
+    
+    else:
+        x2 = (-b - sqrt(delta)) / (2 * a)
+        
+        print(f"x' = {x1}")
+        print(f"x'' = {x2}")
